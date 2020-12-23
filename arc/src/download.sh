b80054a1f273a54cd2314c238dde54527ab1c008
Iliya Zhechev <iliya.zhechev@gmail.com>
2020-12-23 12:10:23 +0200

    Make script for downloading the dataset

--- arc/src/download.sh
+++ arc/src/download.sh
@@ -1,13 +1,12 @@
 ## Download dataset
 
-```sh
+mkdir .data
 cd .data
 
-pip uninstall kaggle -y -
-pip install --upgrade kaggle -
+pip uninstall kaggle -y
+pip install --upgrade kaggle
 kaggle -v # Kaggle API 1.5.10
 
 kaggle competitions download -c abstraction-and-reasoning-challenge
 
 unzip abstraction-and-reasoning-challenge.zip
-```
