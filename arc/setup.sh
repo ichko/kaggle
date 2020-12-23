77ff2115dda755b4110612c78f5a3eb7f81d23c0
Iliya Zhechev <iliya.zhechev@gmail.com>
2020-12-23 12:31:51 +0200

    Move downloading of ds in setup script

--- arc/setup.sh
+++ arc/setup.sh
@@ -1,10 +1,11 @@
 ## Download dataset
 
+mkdir .models
 mkdir .data
-cd .data
 
-pip uninstall kaggle -y
-pip install --upgrade kaggle
+cd .data
+# pip uninstall kaggle -y
+# pip install --upgrade kaggle
 kaggle -v # Kaggle API 1.5.10
 
 kaggle competitions download -c abstraction-and-reasoning-challenge
