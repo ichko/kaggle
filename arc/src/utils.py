948433cf29946479dbe59aa599485a6996ef145c
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-01-13 17:09:31 +0200

    Add plotter of task solution in video

--- arc/src/utils.py
+++ arc/src/utils.py
@@ -1,3 +1,16 @@
 import sys
 
 IS_DEBUG = '--debug' in sys.argv
+
+
+def memoize(f):
+    memo = {}
+
+    def helper(*args, **kwargs):
+        key = str([[str(a) for a in args], kwargs])
+        if key not in memo:
+            memo[key] = f(*args, **kwargs)
+
+        return memo[key]
+
+    return helper
--- arc/src/vis.py
+++ arc/src/vis.py
@@ -3,6 +3,8 @@ from matplotlib import animation, rc
 import matplotlib.pyplot as plt
 import numpy as np
 
+import src.utils as utils
+
 # SRC - <https://www.kaggle.com/teddykoker/training-cellular-automata-part-ii-learning-tasks/notebook>
 pallet = [
     '#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00', '#AAAAAA',
@@ -19,69 +21,41 @@ norm = colors.Normalize(vmin=0, vmax=len(pallet))
 # TODO: Log val set solutions
 # TODO: Log distance to solutions of train and val set
 
-# TODO: Write readme
 # TODO: Try with bigger networks
+
+
+def save_task_vid(path, inputs, outputs, preds_seq, size=2):
+    cols, rows = 4, len(inputs)
+    fig = plt.figure(figsize=(cols * size, rows * size))
+    add_or_get_subplot = utils.memoize(fig.add_subplot)
+
+    def get_actors(preds):
+        diffs = abs(outputs - preds)
+        for r in range(rows):
+            for c, (col, img) in enumerate({
+                    'inputs': inputs,
+                    'outputs': outputs,
+                    'preds': preds,
+                    'diffs': diffs,
+            }.items()):
+                ax = add_or_get_subplot(rows, cols, r * cols + c + 1)
+                ax.set_xticks([])
+                ax.set_yticks([])
+                ax.set_title(f'{col}[{r}]')
+                yield ax.imshow(normalize_board(img[r]), cmap=cmap)
+
+    seq_dim = 1
+    seq_range = range(preds_seq.size(seq_dim))
+    imgs = [list(get_actors(preds_seq[:, i])) for i in seq_range]
+    fig.tight_layout()
+
+    ani = animation.ArtistAnimation(fig, imgs, interval=500, blit=True)
+    ani.save(path)
+    plt.close()
 
 
 def normalize_board(board):
     return board.detach().cpu().numpy()
-
-
-def plot_task_inference(inputs, outputs, preds, size=2):
-    diffs = abs(outputs - preds)
-    cols, rows = 4, len(inputs)
-    fig = plt.figure(figsize=(cols * size, rows * size))
-
-    for r in range(rows):
-        for c, (col, img) in enumerate({
-                'inputs': inputs,
-                'outputs': outputs,
-                'preds': preds,
-                'diffs': diffs,
-        }.items()):
-            ax = fig.add_subplot(rows, cols, r * cols + c + 1)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_title(f'{col}[{r}]')
-            ax.imshow(normalize_board(img[r]), cmap=cmap)
-
-    fig.tight_layout()
-
-    return fig
-
-
-def plot_task(batch, idx, test_preds=None, size=2):
-    task = {k: v[idx] for k, v in batch.items()}
-    if test_preds is not None:
-        task['test_preds'] = test_preds[idx]
-        task['test_diff'] = abs(task['test_outputs'] - test_preds[idx])
-
-    max_len = max(task['train_len'], task['test_len'])
-    should_show_preds = 'test_preds' in task
-
-    rows = max_len
-    cols = 5 + should_show_preds
-    col_names = [
-        'train_inputs',
-        'train_outputs',
-        'test_inputs',
-        'test_outputs',
-    ] + ['test_preds', 'test_diff'] if should_show_preds else []
-
-    fig = plt.figure(figsize=(cols * size, rows * size))
-
-    for r in range(rows):
-        for c, col in enumerate(col_names):
-            if len(task[col]) <= r: continue
-            ax = fig.add_subplot(rows, cols, r * cols + c + 1)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_title(f'{col}[{r}]')
-            ax.imshow(normalize_board(task[col][r]), cmap=cmap)
-
-    fig.tight_layout()
-
-    return fig
 
 
 def plot_grid(grid):
