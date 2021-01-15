50a9902b9179d607b8222299407157fa295e44bd
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-01-15 18:29:28 +0200

    Refactor data script

--- arc/src/data.py
+++ arc/src/data.py
@@ -7,6 +7,8 @@ import torch
 import torch.utils.data as td
 from torch.nn.utils.rnn import pad_sequence
 
+import src.nn_utils as ut
+
 
 def matrix_to_np(obj):
     obj = obj.copy()
@@ -30,7 +32,7 @@ def load_folder(path):
     # file_names = file_names[:8]
 
     if utils.IS_DEBUG:
-        # load only 10 files in debug mode
+        # limit files in debug mode
         file_names = file_names[:32]
 
     for file_name in file_names:
@@ -43,19 +45,6 @@ def load_folder(path):
             result[file_name[:-5]] = json_content
 
     return result
-
-
-def one_hot_channels(inp, max_size):
-    img = np.full(
-        (inp.shape[0], max_size, inp.shape[1], inp.shape[2]),
-        0,
-        dtype=np.uint8,
-    )
-
-    for i in range(max_size):
-        img[:, i] = inp == i
-
-    return img
 
 
 def normalize_matrix_size(matrix):
@@ -101,70 +90,57 @@ def normalize_obj(obj):
     return map_np(normalize_matrix_size, obj)
 
 
-def pad_in_dim(tensor, pad_size, dim, val=0):
-    shape = list(tensor.shape)
-    shape[dim] = pad_size - shape[dim]
-    padding = torch.full(shape, val).to(tensor.device)
-    out = torch.cat([tensor, padding], dim=dim)
-
-    return out
-
-
-def load_data(path, bs, shuffle, device='cpu'):
-    data = load_folder(path)
-    data = normalize_obj(data)
-    tasks = list(data.values())
+def load_arc_data(path, bs, shuffle, device='cpu'):
+    tasks_dict = load_folder(path)
+    tasks_dict = normalize_obj(tasks_dict)
+    tasks = list(tasks_dict.items())
 
     max_train_pairs = 4  # demonstrations are padded to this
     max_test_pairs = 1
-    num_colors = 11  # each color + one id for border
     seq_dim = 0
 
-    def get(selectors, idx, max_pairs):
+    def get(selectors, task, max_pairs):
         train_test, io = selectors
-        data = [t[io] for t in tasks[idx][train_test]]
-        data = data[:max_pairs]
-        data = np.array(data)
-        # data = one_hot_channels(data, max_size=num_colors)
-        # data = data.astype(np.float32)
-        data_len = len(data)
-        data = torch.Tensor(data)
-        data = pad_in_dim(data, pad_size=max_pairs, dim=seq_dim)
+        data = torch.Tensor([t[io] for t in task[train_test]])
+        data = ut.fix_dim_size(data, max_pairs, dim=seq_dim)
         data = data.to(device)
 
-        return data, data_len
+        return data, len(data)
 
     class Dataset(td.Dataset):
         def __len__(self):
             return len(tasks)
 
-        def __getitem__(self, id):
-            train_in, train_len = get(['train', 'input'], id, max_train_pairs)
-            train_out, _ = get(['train', 'output'], id, max_train_pairs)
-            test_in, test_len = get(['test', 'input'], id, max_test_pairs)
-            test_out, _ = get(['test', 'output'], id, max_test_pairs)
+        def __getitem__(self, idx):
+            name, task = tasks[idx]
+            train_in, train_len = \
+                get(['train', 'input'], task, max_train_pairs)
+            train_out, _ = get(['train', 'output'], task, max_train_pairs)
+            test_in, test_len = get(['test', 'input'], task, max_test_pairs)
+            test_out, _ = get(['test', 'output'], task, max_test_pairs)
 
-            # Infer over each demonstration, both in train and test pairs
             all_in = torch.cat([train_in[:train_len], test_in[:test_len]])
             all_out = torch.cat([train_out[:train_len], test_out[:test_len]])
             all_len = train_len + test_len
             max_all_pairs = max_train_pairs + max_test_pairs
 
-            all_in = pad_in_dim(all_in, pad_size=max_all_pairs, dim=seq_dim)
-            all_out = pad_in_dim(all_out, pad_size=max_all_pairs, dim=seq_dim)
+            all_in = ut.fix_dim_size(all_in, max_all_pairs, dim=seq_dim)
+            all_out = ut.fix_dim_size(all_out, max_all_pairs, dim=seq_dim)
 
             return dict(
-                train_inputs=train_in,
-                train_outputs=train_out,
+                name=name,
+                #
+                train_input=train_in,
+                train_output=train_out,
                 train_len=train_len,
                 #
-                test_inputs=test_in,
-                test_outputs=test_out,
+                test_input=test_in,
+                test_output=test_out,
                 test_len=test_len,
                 #
-                all_inputs=all_in,
-                all_outputs=all_out,
-                all_len=all_len,
+                input=all_in,
+                output=all_out,
+                len=all_len,
             ), test_out,
 
     dl = td.DataLoader(
