46726841b00be93a96860445ae04aeee5293cd75
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-01-18 18:08:31 +0200

    Refactor config modeule

--- arc/src/config.py
+++ arc/src/config.py
@@ -1,24 +1,30 @@
 from argparse import Namespace
+from collections import OrderedDict
 
-defaults = dict(
-    eval_interval=20,
-    lr=0.00005,
-    epochs=1_000_000,
-    bs=20,
-)
+defaults = {
+    'eval_interval': 20,
+    'lr': 0.00005,
+    'epochs': 1_000_000,
+    'bs': 20,
+}
 
-configs = dict(
-    hyper_recurrent_cnn=dict(
-        model='hyper_recurrent_cnn',
-        nca_iterations=10,
-        input_channels=11,
-    ),
-    _=None,
-)
+configs = [
+    {
+        'model': 'hyper_recurrent_cnn',
+        'nca_iterations': 10,
+        'input_channels': 11,
+    },
+]
+
+default_id = configs[0]['model']
 
 
 def get_hparams(config_id):
-    config = configs[config_id]
+    if config_id == 'default':
+        config_id = default_id
+
+    [config] = [c for c in configs if c['model'] == config_id]
+
     config_dict = {**defaults, **config}
 
     return Namespace(**config_dict)
