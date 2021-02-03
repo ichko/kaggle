f3f10b18b304dd4c8a65bccbaddb5ca7d95e4e68
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-02-03 15:40:21 +0200

    Refactor metric calculation using the trainer

--- arc/src/metrics.py
+++ arc/src/metrics.py
@@ -7,21 +7,21 @@ import src.data.preprocess as preprocess
 
 # https://www.kaggle.com/c/abstraction-and-reasoning-challenge/overview/evaluation
 # AVG TOP 3 for each task (less is better)
-def arc_eval(model, dataloader, num_iters):
+def arc_eval(optim_iterable):
     error = 0
     length = 0
     num_solved = 0
 
-    for batch in tqdm(dataloader):
-        X, y_batch = preprocess.strict(batch)
+    for info in tqdm(optim_iterable):
         # Currently outputting single prediction per test input
-        y_hat_batch = model(X, num_iters)
+        y_hat_batch = info['y_pred']
+        y_batch = info['y']
+
         length += len(y_batch)
 
         assert y_hat_batch.shape == y_batch.shape
 
         for y, y_hat in zip(y_batch, y_hat_batch):
-
             equal = torch.all(y_hat.int() == y.int()).item()
             num_solved += equal
             error += not equal
