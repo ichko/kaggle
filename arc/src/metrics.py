a1b947b4f4fe5a815110e4b8ac0cc03f4d0e6b1c
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-02-10 14:49:57 +0200

    Metric calculation as map reduce statedull object

--- arc/src/metrics.py
+++ arc/src/metrics.py
@@ -7,23 +7,23 @@ import src.data.preprocess as preprocess
 
 # https://www.kaggle.com/c/abstraction-and-reasoning-challenge/overview/evaluation
 # AVG TOP 3 for each task (less is better)
-def arc_eval(optim_iterable):
-    error = 0
-    length = 0
-    num_solved = 0
+class ArcEval:
+    def __init__(self):
+        self.error = 0
+        self.length = 0
+        self.num_solved = 0
 
-    for info in tqdm(optim_iterable):
+    def push(self, info):
         # Currently outputting single prediction per test input
         y_hat_batch = info['y_pred']
         y_batch = info['y']
-
-        length += len(y_batch)
-
         assert y_hat_batch.shape == y_batch.shape
 
         for y, y_hat in zip(y_batch, y_hat_batch):
             equal = torch.all(y_hat.int() == y.int()).item()
-            num_solved += equal
-            error += not equal
+            self.error += not equal
+            self.length += len(y_batch)
+            self.num_solved += equal
 
-    return error / length, num_solved
+    def reduce(self):
+        return self.error / self.length, self.num_solved
