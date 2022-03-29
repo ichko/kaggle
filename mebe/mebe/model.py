ec079dd2d92e8d5a4ba8edf82afb05be0daca4cd
Iliya Zhechev <iliya.zhechev@gmail.com>
2022-03-30 00:24:22 +0300

    Update model

--- mebe/mebe/model.py
+++ mebe/mebe/model.py
@@ -9,25 +9,56 @@ from mebe.data import MaskedSequencesDataModule
 class TransformerDenoisingModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        encoder_layer = nn.TransformerEncoderLayer(d_model=48, nhead=8)
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=2)
-        self.head = nn.Linear(48, 48)
+        hidden_dim = 20  # 256 // 11
+        self.embed_size = 11 * hidden_dim
+
+        self.encoder_features = nn.Sequential(
+            nn.Linear(48, hidden_dim)
+        )
+
+        self.encoder_entity = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=4), num_layers=2)
+        self.encoder_time = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=4), num_layers=2)
+
+        self.head = nn.Sequential(
+            nn.Linear(hidden_dim, 48),
+            # nn.LeakyReLU(negative_slope=0.02),
+            # nn.Linear(48, 48),
+        )
 
     def forward(self, x):
-        bs, seq, dim = x.shape
-        x = self.encoder(x)
-        x = x.reshape(-1, dim)
+        bs, seq, entity, features, dim = x.shape
+        x = x.view(bs * seq * entity, features * dim)
+        x = self.encoder_features(x)
+        x = x.view(bs * seq, entity, -1)
+        x = self.encoder_entity(x)
         x = self.head(x)
-        x = x.reshape(bs, seq, -1)
+        x = x.view(bs, seq, entity, features, dim)
         return x
+
+    def embed(self, inp):
+        inp[torch.isnan(inp)] = 0
+        batches = torch.chunk(inp, chunks=10, dim=1)
+        outs = []
+        for x in batches:
+            bs, seq, entity, features, dim = x.shape
+            x = x.view(bs * seq * entity, features * dim)
+            x = self.encoder_features(x)
+            x = x.view(bs * seq, entity, -1)
+            x = self.encoder_entity(x)
+            x = x.view(bs, seq, -1)
+
+            outs.append(x)
+
+        outs = torch.cat(outs, dim=1)
+        return outs
 
     def training_step(self, batch, batch_idx):
         names, sequence = batch
 
-        sequence = sequence[0][:1000]
+        sequence = sequence[:, :1000]
         sequence[torch.isnan(sequence)] = 0
-        seq_len, num_flies, _, _ = sequence.shape
-        sequence = sequence.reshape(seq_len, num_flies, -1)
 
         mask_prob = 0.1
         mask = torch.rand_like(sequence) > mask_prob
@@ -41,26 +72,3 @@ class TransformerDenoisingModel(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
-
-
-if __name__ == "__main__":
-    # torch.cuda.empty_cache()
-    model = TransformerDenoisingModel()
-    dm = MaskedSequencesDataModule(bs=1)
-
-    tb_logger = pl.loggers.TensorBoardLogger(".logs/")
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        # monitor="val_loss",
-        monitor="train_loss",
-        dirpath=".checkpoints/",
-        filename="model-{epoch:02d}-{val_loss:.2f}",
-        save_top_k=3,
-        mode="min",
-    )
-
-    trainer = pl.Trainer(gpus=1, logger=tb_logger,
-                         callbacks=[checkpoint_callback])
-    trainer.fit(
-        model=model,
-        train_dataloader=dm.train_dataloader()
-    )
