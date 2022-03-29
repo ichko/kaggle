61b05a48199b95b19d249f98d770fea47b70a96c
Iliya Zhechev <iliya.zhechev@gmail.com>
2022-03-29 10:14:32 +0300

    working lightning training

--- mebe/mebe/model.py
+++ mebe/mebe/model.py
@@ -1,7 +1,12 @@
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+from mebe.data import MaskedSequencesDataModule
 
 
-class Model(nn.Module):
+class TransformerDenoisingModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
         encoder_layer = nn.TransformerEncoderLayer(d_model=48, nhead=8)
@@ -15,3 +20,35 @@ class Model(nn.Module):
         x = self.head(x)
         x = x.reshape(bs, seq, -1)
         return x
+
+    def training_step(self, batch, batch_idx):
+        names, sequence = batch
+
+        sequence = sequence[0]
+        sequence[torch.isnan(sequence)] = 0
+        seq_len, num_flies, _, _ = sequence.shape
+        sequence = sequence.reshape(seq_len, num_flies, -1)
+
+        mask_prob = 0.1
+        mask = torch.rand_like(sequence) > mask_prob
+        masked_batch = sequence * mask
+        out = self(masked_batch)
+        loss = F.mse_loss(out, sequence)
+
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+if __name__ == "__main__":
+    model = TransformerDenoisingModel()
+    dm = MaskedSequencesDataModule(bs=1)
+
+    trainer = pl.Trainer(gpus=1)
+    trainer.fit(
+        model=model,
+        train_dataloader=dm.train_dataloader()
+    )
